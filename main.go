61bc3a70363d37276756a6c448e9df7835d6bc3f
Ankit Shaw <25771394+ankitshaw@users.noreply.github.com>
2023-12-25 04:50:32 +0000

    parse argument and execute run function to print the command

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+// docker 			run image <cmd> <params>
+// go run main.go 	run 	  <cmd> <params>
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("Bad Command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+}
 
 func must(err error) {
 	if err != nil {
